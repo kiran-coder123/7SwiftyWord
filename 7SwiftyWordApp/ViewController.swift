1aca5676c4019012c28c86f00de710348490ce54
Satinder Panesar <satinder@vigsitainteractives.com>
2021-05-13 08:25:30 +0530

    Initial Commit

---
+++ 7SwiftyWordApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7SwiftyWordApp
+//
+//  Created by Satinder Panesar on 5/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
